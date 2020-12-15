e7638644045ffc47a44a570e51fb4ca5e88021a0
tftr <tolgafiratoglu@gmail.com>
2020-12-15 23:47:09 +0300

    Database Connection

--- charta/settings.py
+++ charta/settings.py
@@ -96,6 +96,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'charta',
         'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
